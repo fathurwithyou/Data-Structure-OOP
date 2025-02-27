d5aad658cc3074bef90a36237e941a1bd428a44a
Ellipsis <rizkyfathur326@gmail.com>
2025-02-28 02:45:21 +0700

    update the example

--- example.cpp
+++ example.cpp
@@ -7,14 +7,20 @@ using namespace std;
 int main(){
     Queue<int> q;
     q.push(10);
-    cout << q.front() << endl;
+    Queue<int> cp = q;
+    cout << cp.front() << endl;
 
     Stack<int> s;
     s.push(99);
     cout << s.top() << endl;
 
-    Vector<int> v(10);
+    Vector<Vector<int>> v(10, Vector<int>(10));
+    cout << v.size() << " " << v.get(0).size();
     for(int i = 0; i < v.size(); i++){
-        cout << v.get(i) << " ";
+        for(int j = 0; j < v.get(0).size(); j++){
+            cout << v.get(i).get(j) << " ";
+        }
+        cout << endl;
     }
+    return 0;
 }
