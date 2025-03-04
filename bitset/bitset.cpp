403e164447833d35113f9a40852c84a2a0cacb1c
Ellipsis <rizkyfathur326@gmail.com>
2025-03-04 15:36:47 +0700

    fix: bug for bitset

--- bitset/bitset.cpp
+++ bitset/bitset.cpp
@@ -1,4 +1,32 @@
-#include "Bitset.hpp"
+template <std::size_t N>
+class Bitset<N>::reference {
+ private:
+  uint64_t& word;
+  std::size_t bit_index;
+
+ public:
+  reference(uint64_t& w, std::size_t index) : word(w), bit_index(index) {}
+
+  operator bool() const { return (word & (1ULL << bit_index)) != 0; }
+
+  reference& operator=(bool value) {
+    if (value)
+      word |= (1ULL << bit_index);
+    else
+      word &= ~(1ULL << bit_index);
+    return *this;
+  }
+
+  reference& operator=(const reference& other) {
+    *this = static_cast<bool>(other);
+    return *this;
+  }
+
+  reference& flip() {
+    word ^= (1ULL << bit_index);
+    return *this;
+  }
+};
 
 template <std::size_t N>
 Bitset<N>::Bitset() : bits{} {}
@@ -8,10 +36,8 @@ Bitset<N>::~Bitset() {}
 
 template <std::size_t N>
 bool Bitset<N>::operator==(const Bitset& other) const {
-  for (std::size_t i = 0; i < N; ++i) {
-    if (bits[i] != other.bits[i]) {
-      return false;
-    }
+  for (std::size_t i = 0; i < num_words; ++i) {
+    if (bits[i] != other.bits[i]) return false;
   }
   return true;
 }
@@ -23,24 +49,28 @@ bool Bitset<N>::operator!=(const Bitset& other) const {
 
 template <std::size_t N>
 typename Bitset<N>::reference Bitset<N>::operator[](std::size_t pos) {
-  if (pos >= N) {
-    throw std::out_of_range("Bitset index out of range");
-  }
-  return reference(bits[pos]);
+  if (pos >= N) throw std::out_of_range("Bitset index out of range");
+  std::size_t word_index = pos / 64;
+  std::size_t bit_index = pos % 64;
+  return reference(bits[word_index], bit_index);
 }
 
 template <std::size_t N>
 bool Bitset<N>::test(std::size_t pos) const {
-  if (pos >= N) {
-    throw std::out_of_range("Bitset index out of range");
-  }
-  return bits[pos];
+  if (pos >= N) throw std::out_of_range("Bitset index out of range");
+  std::size_t word_index = pos / 64;
+  std::size_t bit_index = pos % 64;
+  return (bits[word_index] & (1ULL << bit_index)) != 0;
 }
 
 template <std::size_t N>
 bool Bitset<N>::all() const {
-  for (std::size_t i = 0; i < N; ++i) {
-    if (!bits[i]) {
+  for (std::size_t i = 0; i < num_words; ++i) {
+    uint64_t mask = ~0ULL;
+    if (i == num_words - 1 && (N % 64) != 0) {
+      mask = (1ULL << (N % 64)) - 1;
+    }
+    if ((bits[i] & mask) != mask) {
       return false;
     }
   }
@@ -49,10 +79,8 @@ bool Bitset<N>::all() const {
 
 template <std::size_t N>
 bool Bitset<N>::any() const {
-  for (std::size_t i = 0; i < N; ++i) {
-    if (bits[i]) {
-      return true;
-    }
+  for (std::size_t i = 0; i < num_words; ++i) {
+    if (bits[i] != 0) return true;
   }
   return false;
 }
@@ -65,9 +93,11 @@ bool Bitset<N>::none() const {
 template <std::size_t N>
 std::size_t Bitset<N>::count() const {
   std::size_t result = 0;
-  for (std::size_t i = 0; i < N; ++i) {
-    if (bits[i]) {
-      ++result;
+  for (std::size_t i = 0; i < num_words; ++i) {
+    uint64_t x = bits[i];
+    while (x) {
+      result += x & 1ULL;
+      x >>= 1;
     }
   }
   return result;
@@ -80,33 +110,29 @@ std::size_t Bitset<N>::size() const {
 
 template <std::size_t N>
 Bitset<N>& Bitset<N>::operator&=(const Bitset& other) {
-  for (std::size_t i = 0; i < N; ++i) {
-    bits[i] &= other.bits[i];
-  }
+  for (std::size_t i = 0; i < num_words; ++i) bits[i] &= other.bits[i];
   return *this;
 }
 
 template <std::size_t N>
 Bitset<N>& Bitset<N>::operator|=(const Bitset& other) {
-  for (std::size_t i = 0; i < N; ++i) {
-    bits[i] |= other.bits[i];
-  }
+  for (std::size_t i = 0; i < num_words; ++i) bits[i] |= other.bits[i];
   return *this;
 }
 
 template <std::size_t N>
 Bitset<N>& Bitset<N>::operator^=(const Bitset& other) {
-  for (std::size_t i = 0; i < N; ++i) {
-    bits[i] ^= other.bits[i];
-  }
+  for (std::size_t i = 0; i < num_words; ++i) bits[i] ^= other.bits[i];
   return *this;
 }
 
 template <std::size_t N>
 Bitset<N> Bitset<N>::operator~() const {
   Bitset result;
-  for (std::size_t i = 0; i < N; ++i) {
-    result.bits[i] = !bits[i];
+  for (std::size_t i = 0; i < num_words; ++i) result.bits[i] = ~bits[i];
+  if (N % 64 != 0) {
+    uint64_t mask = (1ULL << (N % 64)) - 1;
+    result.bits[num_words - 1] &= mask;
   }
   return result;
 }
@@ -114,30 +140,47 @@ Bitset<N> Bitset<N>::operator~() const {
 template <std::size_t N>
 Bitset<N>& Bitset<N>::operator<<=(std::size_t pos) {
   if (pos >= N) {
-    std::fill(bits, bits + N, false);
-  } else {
-    for (std::size_t i = N - 1; i >= pos; --i) {
-      bits[i] = bits[i - pos];
-    }
-    for (std::size_t i = 0; i < pos; ++i) {
-      bits[i] = false;
-    }
+    std::fill(bits.begin(), bits.end(), 0);
+    return *this;
   }
+  Bitset tmp;
+  std::size_t word_shift = pos / 64;
+  std::size_t bit_shift = pos % 64;
+  for (std::size_t i = num_words; i > 0; --i) {
+    std::size_t src_index =
+        (i - 1 >= word_shift) ? (i - 1 - word_shift) : num_words;
+    uint64_t val = 0;
+    if (src_index < num_words) {
+      val = bits[src_index] << bit_shift;
+      if (bit_shift > 0 && src_index > 0)
+        val |= bits[src_index - 1] >> (64 - bit_shift);
+    }
+    tmp.bits[i - 1] = val;
+  }
+  *this = tmp;
   return *this;
 }
 
 template <std::size_t N>
 Bitset<N>& Bitset<N>::operator>>=(std::size_t pos) {
   if (pos >= N) {
-    std::fill(bits, bits + N, false);
-  } else {
-    for (std::size_t i = 0; i < N - pos; ++i) {
-      bits[i] = bits[i + pos];
-    }
-    for (std::size_t i = N - pos; i < N; ++i) {
-      bits[i] = false;
-    }
+    std::fill(bits.begin(), bits.end(), 0);
+    return *this;
   }
+  Bitset tmp;
+  std::size_t word_shift = pos / 64;
+  std::size_t bit_shift = pos % 64;
+  for (std::size_t i = 0; i < num_words; ++i) {
+    std::size_t src_index = i + word_shift;
+    uint64_t val = 0;
+    if (src_index < num_words) {
+      val = bits[src_index] >> bit_shift;
+      if (bit_shift > 0 && src_index + 1 < num_words)
+        val |= bits[src_index + 1] << (64 - bit_shift);
+    }
+    tmp.bits[i] = val;
+  }
+  *this = tmp;
   return *this;
 }
 
@@ -157,88 +200,87 @@ Bitset<N> Bitset<N>::operator>>(std::size_t pos) const {
 
 template <std::size_t N>
 Bitset<N>& Bitset<N>::set() {
-  for (std::size_t i = 0; i < N; ++i) {
-    bits[i] = true;
+  for (std::size_t i = 0; i < num_words; ++i) bits[i] = ~0ULL;
+  if (N % 64 != 0) {
+    uint64_t mask = (1ULL << (N % 64)) - 1;
+    bits[num_words - 1] = mask;
   }
   return *this;
 }
 
 template <std::size_t N>
 Bitset<N>& Bitset<N>::set(std::size_t pos, bool value) {
-  if (pos >= N) {
-    throw std::out_of_range("Bitset index out of range");
-  }
-  bits[pos] = value;
+  if (pos >= N) throw std::out_of_range("Bitset index out of range");
+  std::size_t word_index = pos / 64;
+  std::size_t bit_index = pos % 64;
+  if (value)
+    bits[word_index] |= (1ULL << bit_index);
+  else
+    bits[word_index] &= ~(1ULL << bit_index);
   return *this;
 }
 
 template <std::size_t N>
 Bitset<N>& Bitset<N>::reset() {
-  for (std::size_t i = 0; i < N; ++i) {
-    bits[i] = false;
-  }
+  std::fill(bits.begin(), bits.end(), 0);
   return *this;
 }
 
 template <std::size_t N>
 Bitset<N>& Bitset<N>::reset(std::size_t pos) {
-  if (pos >= N) {
-    throw std::out_of_range("Bitset index out of range");
-  }
-  bits[pos] = false;
-  return *this;
+  return set(pos, false);
 }
 
 template <std::size_t N>
 Bitset<N>& Bitset<N>::flip() {
-  for (std::size_t i = 0; i < N; ++i) {
-    bits[i] = !bits[i];
+  for (std::size_t i = 0; i < num_words; ++i) bits[i] = ~bits[i];
+  if (N % 64 != 0) {
+    uint64_t mask = (1ULL << (N % 64)) - 1;
+    bits[num_words - 1] &= mask;
   }
   return *this;
 }
 
 template <std::size_t N>
 Bitset<N>& Bitset<N>::flip(std::size_t pos) {
-  if (pos >= N) {
-    throw std::out_of_range("Bitset index out of range");
-  }
-  bits[pos] = !bits[pos];
+  if (pos >= N) throw std::out_of_range("Bitset index out of range");
+  std::size_t word_index = pos / 64;
+  std::size_t bit_index = pos % 64;
+  bits[word_index] ^= (1ULL << bit_index);
   return *this;
 }
 
 template <std::size_t N>
 std::string Bitset<N>::to_string() const {
   std::string result;
+  result.reserve(N);
   for (std::size_t i = N; i > 0; --i) {
-    result += bits[i - 1] ? '1' : '0';
+    std::size_t pos = i - 1;
+    std::size_t word_index = pos / 64;
+    std::size_t bit_index = pos % 64;
+    result.push_back((bits[word_index] & (1ULL << bit_index)) ? '1' : '0');
   }
   return result;
 }
 
 template <std::size_t N>
 unsigned long Bitset<N>::to_ulong() const {
-  if (N > sizeof(unsigned long) * 8) {
+  if (N > sizeof(unsigned long) * 8)
     throw std::overflow_error("Bitset to_ulong overflow");
-  }
   unsigned long result = 0;
-  for (std::size_t i = 0; i < N; ++i) {
-    if (bits[i]) {
-      result |= (1UL << i);
-    }
+  for (std::size_t pos = 0; pos < N; ++pos) {
+    if (test(pos)) result |= (1UL << pos);
   }
   return result;
 }
 
 template <std::size_t N>
 unsigned long long Bitset<N>::to_ullong() const {
-  if (N > sizeof(unsigned long long) * 8) {
+  if (N > sizeof(unsigned long long) * 8)
     throw std::overflow_error("Bitset to_ullong overflow");
-  }
   unsigned long long result = 0;
-  for (std::size_t i = 0; i < N; ++i) {
-    if (bits[i]) {
-      result |= (1ULL << i);
-    }
+  for (std::size_t pos = 0; pos < N; ++pos) {
+    if (test(pos)) result |= (1ULL << pos);
   }
   return result;
 }
@@ -274,8 +316,9 @@ template <std::size_t N>
 std::istream& operator>>(std::istream& is, Bitset<N>& b) {
   std::string str;
   is >> str;
-  for (std::size_t i = 0; i < N; ++i) {
-    b.set(N - 1 - i, str[i] == '1');
-  }
+  if (str.size() != N)
+    throw std::invalid_argument(
+        "Input string length does not match Bitset size");
+  for (std::size_t i = 0; i < N; ++i) b.set(N - 1 - i, str[i] == '1');
   return is;
 }
--- bitset/bitset.hpp
+++ bitset/bitset.hpp
@@ -1,54 +1,56 @@
 #ifndef BITSET_HPP
 #define BITSET_HPP
 
+#include <array>
 #include <cstddef>
 #include <stdexcept>
+#include <cstdint>
 #include <string>
+#include <algorithm>
+#include <iostream>
 
-// Bitset class implementation based on
-// https://en.cppreference.com/w/cpp/utility/bitset
 template <std::size_t N>
 class Bitset {
- private:
-  std::array<uint64_t, (N + 63) / 64> bits;
+private:
+    static constexpr std::size_t num_words = (N + 63) / 64;
+    std::array<uint64_t, num_words> bits;
 
- public:
-  class reference;
+public:
+    class reference;
 
-  Bitset();
-  ~Bitset();
+    Bitset();
+    ~Bitset();
 
-  bool operator==(const Bitset& other) const;
-  bool operator!=(const Bitset& other) const;
+    bool operator==(const Bitset& other) const;
+    bool operator!=(const Bitset& other) const;
 
-  reference operator[](std::size_t pos);
-  bool test(std::size_t pos) const;
-  bool all() const;
-  bool any() const;
-  bool none() const;
-  std::size_t count() const;
+    reference operator[](std::size_t pos);
+    bool test(std::size_t pos) const;
+    bool all() const;
+    bool any() const;
+    bool none() const;
+    std::size_t count() const;
+    std::size_t size() const;
 
-  std::size_t size() const;
+    Bitset& operator&=(const Bitset& other);
+    Bitset& operator|=(const Bitset& other);
+    Bitset& operator^=(const Bitset& other);
+    Bitset operator~() const;
+    Bitset& operator<<=(std::size_t pos);
+    Bitset& operator>>=(std::size_t pos);
+    Bitset operator<<(std::size_t pos) const;
+    Bitset operator>>(std::size_t pos) const;
 
-  Bitset& operator&=(const Bitset& other);
-  Bitset& operator|=(const Bitset& other);
-  Bitset& operator^=(const Bitset& other);
-  Bitset operator~() const;
-  Bitset& operator<<=(std::size_t pos);
-  Bitset& operator>>=(std::size_t pos);
-  Bitset operator<<(std::size_t pos) const;
-  Bitset operator>>(std::size_t pos) const;
+    Bitset& set();
+    Bitset& set(std::size_t pos, bool value = true);
+    Bitset& reset();
+    Bitset& reset(std::size_t pos);
+    Bitset& flip();
+    Bitset& flip(std::size_t pos);
 
-  Bitset& set();
-  Bitset& set(std::size_t pos, bool value = true);
-  Bitset& reset();
-  Bitset& reset(std::size_t pos);
-  Bitset& flip();
-  Bitset& flip(std::size_t pos);
-
-  std::string to_string() const;
-  unsigned long to_ulong() const;
-  unsigned long long to_ullong() const;
+    std::string to_string() const;
+    unsigned long to_ulong() const;
+    unsigned long long to_ullong() const;
 };
 
 template <std::size_t N>
@@ -66,4 +68,6 @@ std::ostream& operator<<(std::ostream& os, const Bitset<N>& b);
 template <std::size_t N>
 std::istream& operator>>(std::istream& is, Bitset<N>& b);
 
-#endif
+#include "Bitset.cpp"
+
+#endif 
